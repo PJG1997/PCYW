9ac17f9baf24744c32786b90fd38f2fa21d0675f
cyj <1056159965@qq.com>
2017-12-31 08:44:00 +0800

    配送点完成

--- ssm_Wuliu/src/main/java/com/yc/biz/ShippointBiz.java
+++ ssm_Wuliu/src/main/java/com/yc/biz/ShippointBiz.java
@@ -12,4 +12,19 @@ public interface ShippointBiz {
 	 */
 	public List<Shippoint> findAllShippoint();
 	
+	/*
+	 * 根据条件筛查配送点信息*/
+	public List<Shippoint> getShippointInfo(Shippoint sp);
+	
+	/*
+	 * 更新配送点信息*/
+	public void updateShippointInfo(Shippoint sp);
+	
+	/*
+	 * 删除配送点*/
+	public void delmanyShippoint(List list);
+	
+	/*
+	 * 添加配送点*/
+	public void addShippoint(Shippoint sp);
 }
