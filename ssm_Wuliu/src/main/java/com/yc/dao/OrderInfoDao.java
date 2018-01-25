5f87b800e66d70d84e19c851121065054c361f8c
pengjinguo <pengjinguo@DESKTOP-ANS6UAP.lan>
2018-01-25 15:48:22 +0800

    订单的添加

---
+++ ssm_Wuliu/src/main/java/com/yc/dao/OrderInfoDao.java
@@ -0,0 +1,15 @@
+package com.yc.dao;
+
+import java.util.List;
+
+import com.yc.bean.Orderinfo;
+
+public interface OrderInfoDao {
+
+	
+	/**
+	 * @param listOrderInfo
+	 * @return
+	 */
+	public int addOrderInfoDao(Orderinfo listOrderInfo);
+}
