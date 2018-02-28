d8f98948a4a1d0b6d2af1b6ddabcea261ebeb290
ysy0021 <453952011@qq.com>
2018-02-28 14:23:08 +0800

    修改

--- ssm_Wuliu/src/main/java/com/yc/controller/Order1Controller.java
+++ ssm_Wuliu/src/main/java/com/yc/controller/Order1Controller.java
@@ -134,17 +134,7 @@ public class Order1Controller {
 		return jsonModel;
 	}
 	
-	@RequestMapping("findOrder.action")
-	public @ResponseBody JsonModel findOrderForOrdId(Order1 order1){
-		try{
-			jsonModel.setObj(order1Biz.findAllOrderToPage(order1));
-			jsonModel.setCode(1);
-		}catch(Exception e){
-			jsonModel.setCode(0);
-		}
-		return jsonModel;
-		
-	}
+
 	@RequestMapping("delOrder.action")
 	@ResponseBody
 	public JsonModel delOrder(@RequestParam(name="stringOsid") String stringOsid){
